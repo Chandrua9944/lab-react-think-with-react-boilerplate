c46c0b6862b04f1f2b49960d5f71de1af632f44d
unknown <chandrua9944@gmail.com>
2024-05-27 17:06:28 +0530

    done

---
+++ app-name/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+//import Home from './Home'
+function App() {
+  return (
+    <>
+      <p>hello kalvium</p>
+    </>
+  )
+}
+
+export default App
